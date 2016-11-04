ab5db588ab9929f0f21a9ce09a28bb4347df191c
Keitaro0408 <kei.kotani77@gmai.com>
2016-11-04 14:27:03 +0900

    no message

--- Directx11Sample/Directx11Sample/PixelShader.hlsl
+++ Directx11Sample/Directx11Sample/PixelShader.hlsl
@@ -15,10 +15,5 @@ float4 ps_main(pixcelIn IN) : SV_Target
 	//ÉJÉâÅ[ÇÇªÇÃÇ‹Ç‹ï‘Ç∑
 	OUT.col = txDiffuse.Sample(samLinear, IN.tex);
 
-	//è≠Çµà√Ç≠Ç∑ÇÈ
-	OUT.col.r -= 0.3f;
-	OUT.col.g -= 0.3f;
-	OUT.col.b -= 0.3f;
-
 	return OUT.col;
 }
